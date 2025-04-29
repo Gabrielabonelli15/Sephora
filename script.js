488a02b0239e055913bba44e2138f80f11607599
Gabriela Bonelli <gabriela_silva21@estudante.sesisenai.org.br>
2025-04-29 12:58:25 -0300

    add

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const tabs = document.querySelectorAll('.tabs span');
+function showResults(preference) {
+    document.getElementById('quiz').style.display = 'none';
+    document.getElementById('result').style.display = 'block';
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    tabs.forEach(t => t.classList.remove('active'));
-    tab.classList.add('active');
-    // Lógica adicional aqui, como mostrar "Marcas" no lugar de "Categorias"
-  });
-});
+    if (preference === 'leve') {
+        document.getElementById('product1').innerText = 'Base leve e natural';
+        document.getElementById('product2').innerText = 'Batom nude';
+        document.getElementById('product3').innerText = 'Máscara de cílios suave';
+    } 
+    
+    else {
+        document.getElementById('product1').innerText = 'Base de alta cobertura';
+        document.getElementById('product2').innerText = 'Batom vermelho intenso';
+        document.getElementById('product3').innerText = 'Sombra escura e marcante';
+    }
+}
